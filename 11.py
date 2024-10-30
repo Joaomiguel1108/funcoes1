8ff0468625d24335cb38011445305f98d3fe26a7
João Miguel <joaomiguelmorais1108@gmail.com>
2024-10-30 17:54:08 -0300

    Create 11.py

---
+++ 11.py
@@ -0,0 +1,9 @@
+a = float(input("Digite um número"))
+b = float(input("Digite um número"))
+c = float(input("Digite um número"))
+    
+def media(a,b,c):
+   
+    print (a+b+c/3)
+    
+media(a,b,c)
